b74c54bc4bffd6e8409f192a9f70ca3dc705817c
Gweltaz Mary <gweltazmary@gmail.com>
2020-10-07 17:09:05 +0200

    Starting server implementation

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+//  Importing packages
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 
+// Loading config env variablesnpm
+dotenv.config({ path: './config/config.env' });
+// Starting the server
+const app = express();
+
+// HTTP request logger on dev mode only
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
+// Enabling static files
+app.use(express.static('public'));
+
+//  Serverside template engine
+app.set('view engine', 'ejs');
+
+// Server listening PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port : ${PORT}`
+  )
+);
+
+// Setting Routes
+
+// Homepage
+const homePagePath = require('./routes/homepage.js');
+app.use('/', homePagePath);
---
+++ routes/homepage.js
@@ -0,0 +1,10 @@
+//  Importing packages
+const express = require('express');
+const router = express.Router();
+
+// Homepage ROOT
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+module.exports = router;
